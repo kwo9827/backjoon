8cb753fe1dfbbd3a69a111a477e0c09003a96ce4
오승열 <tmdduf785@naver.com>
2025-09-26 19:18:34 +0900

    1002(2)

--- silver/1002/1002.py
+++ silver/1002/1002.py
@@ -6,7 +6,7 @@ T = int(input())
 for _ in range(T):
     x1, y1, r1, x2, y2, r2 = map(int,input().split())
 
-    if x1 == y2 and y1 == y2:
+    if x1 == x2 and y1 == y2:
         if r1 == r2:
             print(-1)
         else:
